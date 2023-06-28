ccfe3705756576fb794ba8ea9576e0c91bce424f
Pfulcher26 <pfulcher26@gamil.com>
2023-06-28 14:14:21 -0700

    Creats two separate constructors for milesValue and cashValue.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,10 +3,15 @@ public class RewardValue {
     private double cashValue;
     private double milesValue;
 
-    public RewardValue(double cashValue, double milesValue) {
+    public RewardValue(double milesValue) {
+
+        this.milesValue = milesValue;
+
+    }
+
+    public RewardValue(double cashValue) {
 
         this.cashValue = cashValue;
-        this.milesValue = milesValue;
 
     }
 
