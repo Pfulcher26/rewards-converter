cbd8d9fda1226a585191dd3152b9f469c5f194ad
Pfulcher26 <pfulcher26@gamil.com>
2023-06-28 14:24:25 -0700

    Refactors code to combine separate constructors into one and provides code for getCashValue and getMilesValue methods.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,16 @@
 public class RewardValue {
-
     private double cashValue;
     private double milesValue;
 
-    public RewardValue(double milesValue) {
-
-        this.milesValue = milesValue;
-
-    }
-
-    public RewardValue(double cashValue) {
-
+    public RewardValue(double cashValue, double milesValue) {
         this.cashValue = cashValue;
-
+        this.milesValue = milesValue;
+    }
+    public double getCashValue() {
+        return milesValue * 0.0035;
     }
 
-
-
-
+    public double getMilesValue() {
+        return cashValue / 0.0035;
+    }
 }
