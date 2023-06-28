674c6a2e83dd38fc6912aa2e11b68c4bea22928d
Pfulcher26 <pfulcher26@gamil.com>
2023-06-28 14:35:41 -0700

    Refactors code based on implementation of RewardValue in the RewardsConverter class to convert the given argument to both cash and miles in the constructor itself.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,22 @@
 public class RewardValue {
     private double cashValue;
     private double milesValue;
+
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
     }
 
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
     }
+
     public double getCashValue() {
-        return milesValue * 0.0035;
+        return cashValue;
     }
 
     public double getMilesValue() {
-        return cashValue / 0.0035;
+        return milesValue;
     }
 }
